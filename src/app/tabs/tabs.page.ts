714252e54558df9eb23d19df4b51d6d01c09d223
Mohammad Ahsan <nahsan06@yahoo.com>
2026-04-09 19:13:15 -0400

    commit in new branch

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -2,7 +2,7 @@ import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
-
+///
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
